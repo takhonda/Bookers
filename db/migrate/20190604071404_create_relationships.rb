5dd2b0d8e5bbc6a135f2fca1b403c90dffd97441
takhonda <thonda745@gmail.com>
2019-06-23 12:02:50 +0000

    フォロー機能修正

--- db/migrate/20190604071404_create_relationships.rb
+++ db/migrate/20190604071404_create_relationships.rb
@@ -3,11 +3,12 @@ class CreateRelationships < ActiveRecord::Migration[5.0]
     create_table :relationships do |t|
       t.integer :follower_id
       t.integer :following_id
-      
+
       t.timestamps null: false
-  end
-      add_index :relationships, :follower_id
-      add_index :relationships, :following_id
-      add_index :relationships, [:follower_id, :following_id], unique: true
+    end
+    
+    add_index :relationships, :follower_id
+    add_index :relationships, :following_id
+    add_index :relationships, [:follower_id, :following_id], unique: true
   end
 end
